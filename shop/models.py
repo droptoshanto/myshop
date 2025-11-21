4dc8b63dd3460bc3fc006271f0235ed643e26ac4
droptoshanto <you@example.comdroptoshanto@gmail.com>
2025-11-22 02:02:35 +0600

    s

---
+++ shop/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.slug])
---
+++ shop/views.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.filter(available=True)
+    return render(request, 'shop/product_list.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, available=True)
+    return render(request, 'shop/product_detail.html', {'product': product})
